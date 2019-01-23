c9aea215784b48ed2f9309e6a48d19bfac1ab2d9
Luc Patiny <luc@patiny.com>
2019-01-23 18:05:58 +0100

    build

--- tips/build.js
+++ tips/build.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const fs = require('fs');
+
+const fsExtra = require('fs-extra');
+
 const { join } = require('path');
 
 const exec = require('child-process-promise').exec;
@@ -15,14 +18,22 @@ async function build() {
   //  var stderr = result.stderr;
 
   const homedir = join(__dirname, 'src');
+  const sourceLayouts = join(homedir, 'template', '_layouts');
   let dirs = fs.readdirSync(homedir).filter((item) => item.length === 32);
   for (let dir of dirs) {
+    console.log('Processing: ', dir);
     let summary = join(homedir, dir, 'SUMMARY.md');
     let readme = join(homedir, dir, 'README.md');
     if (fs.existsSync(summary) && fs.existsSync(readme)) {
+      // we temporary copy the layout
+      let layouts = join(homedir, dir, '_layouts');
+      if (fs.existsSync(layouts)) fsExtra.removeSync(layouts);
+      fs.mkdirSync(layouts);
+      fsExtra.copySync(sourceLayouts, layouts);
       await exec(
         `cd tips; node ../node_modules/gitbook-cli/bin/gitbook.js build -- src/${dir} pages/${dir}`
       );
+      fsExtra.removeSync(layouts);
     }
   }
 }
